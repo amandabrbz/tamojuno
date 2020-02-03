7970415ee18001c6d1d578e7d7041b39d79b647f
Amanda Barboza <amanda.brbz@hotmail.com>
2020-02-02 22:10:08 -0300

    amanha vai

--- src/components/Liked/index.js
+++ src/components/Liked/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-import { Container, Like } from "./styles";
+import { Container, Like, Container2 } from "./styles";
+import cta from "../../assets/cta.png";
 
 export default class Liked extends Component {
   render() {
     return (
       <Container>
         <Like>Gostou?</Like>
+        <Container2>
+          <img src={cta} alt="" />
+        </Container2>
       </Container>
     );
   }
--- src/components/Liked/styles.js
+++ src/components/Liked/styles.js
@@ -1,16 +1,21 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.section`
   position: absolute;
-  width: 960px;
+  width: 50%;
   height: 450px;
   left: 0px;
   top: 1509px;
   background-color: #00155f;
 `;
 
-export const Like = styled.div`
+export const Like = styled.h3`
   font-size: 32px;
   line-height: 32px;
   color: #ffffff;
+`;
+
+export const Container2 = styled.section`
+  position: absolute;
+  width: 50%;
 `;
