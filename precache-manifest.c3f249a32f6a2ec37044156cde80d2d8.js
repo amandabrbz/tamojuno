1d82c82727274803028091e652897c14062f06c7
Amanda Barboza <amanda.brbz@hotmail.com>
2020-02-04 09:02:30 -0300

    Updates

---
+++ precache-manifest.c3f249a32f6a2ec37044156cde80d2d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6921a3a741f4b50250620645a77f8c3d",
+    "url": "/tamojuno/index.html"
+  },
+  {
+    "revision": "fe97bd9fc9b73fb49de0",
+    "url": "/tamojuno/static/css/main.3b26859d.chunk.css"
+  },
+  {
+    "revision": "be2e8cbb470632fd8b61",
+    "url": "/tamojuno/static/js/2.e0c6f30c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/tamojuno/static/js/2.e0c6f30c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe97bd9fc9b73fb49de0",
+    "url": "/tamojuno/static/js/main.8cd1990a.chunk.js"
+  },
+  {
+    "revision": "21cc43f039836ab8579a",
+    "url": "/tamojuno/static/js/runtime-main.c4da10c6.js"
+  },
+  {
+    "revision": "1fa3e3e24052b6f44c0de0c36adbe016",
+    "url": "/tamojuno/static/media/banner.1fa3e3e2.png"
+  },
+  {
+    "revision": "6cc18917f68664da19d535e6769db05a",
+    "url": "/tamojuno/static/media/cobrancas.6cc18917.png"
+  },
+  {
+    "revision": "c441f71b9419f1cc277f88cfac948c29",
+    "url": "/tamojuno/static/media/cta.c441f71b.png"
+  },
+  {
+    "revision": "6e06a8f24278bb614499b59cfca15bb9",
+    "url": "/tamojuno/static/media/juno.6e06a8f2.svg"
+  }
+]);
